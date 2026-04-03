9034a4cef1514af9461e7e93b52937a53e48a2bf
Kota Morishita <kouta.morishita@gmail.com>
2026-04-03 15:27:01 -0400

    Update mods to 1.6

--- Casks/mods.rb
+++ Casks/mods.rb
@@ -1,6 +1,6 @@
 cask "mods" do
-  version "1.5.1"
-  sha256 "6e097f2deecc0efff2133937f078e1d3fe8b714b61b3be29eeb40080a4880036"
+  version "1.6"
+  sha256 "81bf4fb1864cc4b2e97bdbae669ca70d60d6a4484e46925bb798a882d5e0f8d6"
 
   url "https://github.com/kotamorishi/mods/releases/download/v#{version}/Mods-#{version}.zip"
   name "Mods"
